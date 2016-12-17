8dd2c833ee6d745b9a571a4a3768b2af952ce355
ectest <1138493417@qq.com>
2016-12-17 15:39:34 +0800

    build

--- js/app.js
+++ js/app.js
@@ -8,12 +8,13 @@ new Vue({
         },
         methods: {
             newConfig: function () {
+                $('.btn.btn-default').attr("disabled", true)
                 layer.msg('解析中', {
                     time: 0, //不自动关闭
                     shadeClose: false,
                     shade: 0.3
                 });
-                $('.btn.btn-default').attr("disabled", true)
+                
                 this.$http.get('api.php?email=' + this.email + '&password=' + this.password, {emulateJSON: true}).then(
                         function (response) {
                             if (response.body.indexOf('Failed') != -1) {
@@ -25,17 +26,17 @@ new Vue({
                             var result = JSON.parse(response.body)
                             this.configs = []
                             for (var i = 0; i < result.data.length; i++) {
-                                var ssObj = result.data[i].attributes.port_mappings
-                                var jsonText = JSON.stringify(ssObj)
-                                var json = eval('(' + jsonText + ')')
-                                if (json != null) {
-                                    for (var j = 0; j < json.length; j++) {
-                                        for (var k = 0; k < json[j].length; k++) {
-                                            var ssConfigs = {}
-                                            ssConfigs.service_port = json[j][k].service_port
-                                            ssConfigs.container_port = json[j][k].container_port
-                                            ssConfigs.host = this.reHost(json[j][k].host)
-                                            this.configs.push(ssConfigs)
+                                var mappingsObj = result.data[i].attributes.port_mappings
+                                var mappingText = JSON.stringify(mappingsObj)
+                                var  mappingJson = eval('(' + mappingText + ')')
+                                if (mappingJson) {
+                                    for (var j = 0; j < mappingJson.length; j++) {
+                                        for (var k = 0; k < mappingJson[j].length; k++) {
+                                            var mappingResult = {}
+                                            mappingResult.service_port = mappingJson[j][k].service_port
+                                            mappingResult.container_port = mappingJson[j][k].container_port
+                                            mappingResult.host = this.reHost(mappingJson[j][k].host)
+                                            this.configs.push(mappingResult)
                                         }
                                     }
                                 }
