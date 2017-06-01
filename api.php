b8a7c2ca04277f169caf9d7ca4c63ea997172076
ECH2O <zengmeng@hrbeu.edu.cn>
2017-06-01 15:23:36 +0800

    Update api.php

--- api.php
+++ api.php
@@ -66,49 +66,4 @@ function get_content($cookie)
     
 }
 
-
-/*
-
-
-function curlPost($url, $data = array(), $timeout = 30, $CA = true){
-
-    $cacert = getcwd() . '/cacert.pem'; //CA根证书
-    $SSL = substr($url, 0, 8) == "https://" ? true : false;
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout-2);
-    if ($SSL && $CA) {
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);   // 只信任CA颁布的证书
-        curl_setopt($ch, CURLOPT_CAINFO, $cacert); // CA根证书（用来验证的网站证书是否是CA颁布）
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); // 检查证书中是否设置域名，并且是否与提供的主机名匹配
-    } else if ($SSL && !$CA) {
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 信任任何证书
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1); // 检查证书中是否设置域名
-    }
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:')); //避免data数据过长问题
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data)); //data with URLEncode
-
-    $ret = curl_exec($ch);
-    var_dump(curl_error($ch));  //查看报错信息
-
-    curl_close($ch);
-    return $ret;
-}
-
-
-
-    curlPost($url,$data2);*/
-
-
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array());
-//curl_setopt($ch, CURLOPT_USERAGENT, $UserAgent);
-#curl_setopt ($ch, CURLOPT_REFERER, "https://app.arukas.io");
-//curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);//超时
-//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);//禁用ssl
-#curl_setopt($ch, CURLOPT_POSTFIELDS , http_build_query($data));
 ?>
